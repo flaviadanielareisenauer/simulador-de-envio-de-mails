d21388fb3e1f836daa709fb81a645403b484057e
flaviadanielareisenauer <flaviareisenauer@gmail.com>
2022-03-15 20:48:36 -0300

    agregue mensajes de errores

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const btnEnviar = document.querySelector("#enviar");
+const formulario = document.querySelector("#enviar-mail");
 
 const email = document.querySelector("#email");
 const asunto = document.querySelector("#asunto");
@@ -21,7 +22,29 @@ function iniciarApp() {
 function validarFormulario(event) {
   if (event.target.value.length > 0) {
     console.log("Si hay algo");
-  } else {   
+  } else {
     event.target.classList.add("border", "border-red-500");
+
+    mostrarError();
+  }
+}
+
+function mostrarError() {
+  const mensajeError = document.createElement("p");
+  mensajeError.textContent = "Todos los campos son obligatorios";
+  mensajeError.classList.add(
+    "border",
+    "border-red-500",
+    "background-red-100",
+    "text-red-500",
+    "p-3",
+    "mt-5",
+    "text-center",
+    "error"
+  );
+
+  const errores = document.querySelectorAll(".error");
+  if (errores.length === 0) {
+    formulario.appendChild(mensajeError);
   }
 }
